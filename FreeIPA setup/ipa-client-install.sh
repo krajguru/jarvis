e7740cb2304b39efe2cce3f3a833075986185c11
Kunal Rajguru <krajguru@users.noreply.github.com>
2018-07-11 16:15:11 +0530

    Update ipa-client-install.sh

--- FreeIPA setup/ipa-client-install.sh
+++ FreeIPA setup/ipa-client-install.sh
@@ -2,5 +2,16 @@
 
 yum install -y ipa-client
 service messagebus restart
-ipa-client-install --password 'secret#1' --domain `hostname -d` --server `grep node2 /etc/hosts | awk '{print $2}'` --principal admin@`hostname -d| awk '{print toupper($0)}'` --password secret#1 --unattended
+
+## Edit resolv.conf to point to IPA server's DNS 
+
+echo "search `hostname -d`" > /tmp/resolv.conf ; echo "nameserver `grep node2 /etc/hosts | awk -F ' ' '{print $1}'`" >> /tmp/resolv.conf ; cat /tmp/resolv.conf > /etc/resolv.conf ; echo "cat /tmp/resolv.conf > /etc/resolv.conf" >> /etc/rc.local
+chmod +x /etc/rc.d/rc.local
+
+## Setup IPA Client
+
+ipa-client-install --password 'secret#1' --domain `hostname -d` --server `grep node2 /etc/hosts | awk '{print $2}'` --principal admin@`hostname -d| awk '{print toupper($0)}'` --password secret#1 --enable-dns-updates --unattended
+
+## Change the Kerberos cache to FILE cache from KEYRING
+
 sed -i.bak 's/.*default_ccache_name.*/default_ccache_name = FILE:\/tmp\/krb5cc_%{uid}/' /etc/krb5.conf
