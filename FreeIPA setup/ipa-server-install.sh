44f26cc3be40aa66796bb0c49b1cb68d0e0adae1
Kunal Rajguru <krajguru@users.noreply.github.com>
2019-04-04 17:51:09 +1100

    Update ipa-server-install.sh

--- FreeIPA setup/ipa-server-install.sh
+++ FreeIPA setup/ipa-server-install.sh
@@ -60,9 +60,19 @@ case "$OS_VERSION" in
            ipa role-add-privilege hadoopadminrole --privileges="Service Administrators"
            ipa role-add-member hadoopadminrole --users=hadoopadmin
            
+           
+           
         ## Edit resolv.conf to point to IPA server's DNS
         
-           echo "service sssd restart" > /tmp/dns.sh ; echo "echo \"search `hostname -d`\" > /etc/resolv.conf" >> /tmp/dns.sh ; echo "echo \"nameserver \`grep node1 /etc/hosts | awk -F ' ' '{print \$1}' | head -n1 \`\" >> /etc/resolv.conf" >> /tmp/dns.sh ; chmod 777 /tmp/dns.sh
+           echo "s1=\$(hostname -d)" >> /tmp/dns.sh
+           echo "s2=\$(grep search /etc/resolv.conf | awk -F ' ' '{print $2}')" >> /tmp/dns.sh
+           echo "n1=\$(grep node1 /etc/hosts | awk -F ' ' '{print $1}' | head -n1)" >> /tmp/dns.sh
+           echo "n2=\$(grep nameserver /etc/resolv.conf | awk -F ' ' '{print $2}')" >> /tmp/dns.sh
+           echo "if [[ \$s1 == \$s2 && \$n1 == \$n2 ]]" >> /tmp/dns.sh
+           echo "then" >> /tmp/dns.sh
+           echo "exit 0" >> /tmp/dns.sh
+           echo "else" >> /tmp/dns.sh
+           echo "service sssd restart" >> /tmp/dns.sh ; echo "echo \"search `hostname -d`\" > /etc/resolv.conf" >> /tmp/dns.sh ; echo "echo \"nameserver \`grep node1 /etc/hosts | awk -F ' ' '{print \$1}' | head -n1 \`\" >> /etc/resolv.conf" >> /tmp/dns.sh ; chmod 777 /tmp/dns.sh
            echo "domain=\$(hostname -d)" >> /tmp/dns.sh
            echo "host=\$(hostname -s)" >> /tmp/dns.sh
            echo "a_rec=\$(nslookup \`hostname -s\` | tail -n2 | head -n1 | awk -F ' ' '{print \$2}')" >> /tmp/dns.sh
@@ -70,18 +80,23 @@ case "$OS_VERSION" in
            echo "echo secret#1 | kinit admin" >> /tmp/dns.sh
            echo "ipa dnsrecord-mod \$domain. \$host --a-rec=\$a_rec --a-ip-address=\$a_ip_addr" >> /tmp/dns.sh
            echo "exit 0" >> /tmp/dns.sh
+           echo "fi" >> /tmp/dns.sh
+           
            chmod 777 /tmp/dns.sh
            
            ##sed -i '/systemctl start sshd/ash /tmp/dns.sh' /start
-           ##sh /tmp/dns.sh
+           sh /tmp/dns.sh
         
+           ## Crontab to run it on every reboot 
+           
+           echo "* * * * * root /tmp/dns.sh" >> /etc/crontab 
         ## Setup a systemd servive                     
          
-           echo -e "[Unit]\nDescription=Update IPA DNS Records after IP change\nAfter=ipa.service\n\n[Service]\nType=simple\nUser=root\nExecStart=/bin/bash /tmp/dns.sh\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/ipa-dns-update.service
-           chmod 664 /etc/systemd/system/ipa-dns-update.service
-           systemctl daemon-reload
-           systemctl enable ipa-dns-update.service
-           systemctl start ipa-dns-update.service
+           ##echo -e "[Unit]\nDescription=Update IPA DNS Records after IP change\nAfter=ipa.service\n\n[Service]\nType=simple\nUser=root\nExecStart=/bin/bash /tmp/dns.sh\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/ipa-dns-update.service
+           ##chmod 664 /etc/systemd/system/ipa-dns-update.service
+           ##systemctl daemon-reload
+           ##systemctl enable ipa-dns-update.service
+           ##systemctl start ipa-dns-update.service
            
            #rm -rf $USERS $GROUPS /tmp/ipa-add-users.sh /tmp/ipa-add-groups.txt
         ;;
